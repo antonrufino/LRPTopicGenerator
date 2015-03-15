951961a9334c36b444ce50edb4b15446d3224863
Anton Rufino <anton.vongola@gmail.com>
2015-03-15 16:57:29 +0800

    Finished the JS.

---
+++ constants.js
@@ -0,0 +1,17 @@
+var templates = [
+    ["Analysis of the effects of ", " on ", ""],
+    ["The Pros and Cons of replacing ", " with ", ""],
+    ["Why the Philippines should ban ", " and ", ""],
+    ["Why the Philippines should legalize ", " and ", ""],
+    ["Should the government pay attention to ", " instead of ", ""],
+    ["Solving the ", " problem using ", ", ", " and deterministic numerical methods"],
+    ["Edi Wow: An analysis of elitism in the academic fields of ", " and ", ""],
+    ["A mathematical analysis of the effects of ", " on ", ""],
+    ["Constructing computational models of ", " using ", ""],
+    ["An analysis of ", ""],
+    ["On the benefits of ", ""]
+]
+
+var topics = ["PNoy", "Vice Ganda", "That Thing Called Tadhana", "Fifty Shades of Gray", 
+    "piracy", "hugot", "LRP", "CMSC 56", "CMSC 11", "MATH 17", "my hopes and dreams", 
+    "communism", "\"fuck this shit\"", "PewDiePie", "Our Lord and Savior, Tatsuya Stu (MTSBWY)"]
