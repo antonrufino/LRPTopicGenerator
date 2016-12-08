f552dfd5db4cfdd5fbc49a0153a692b4be94e60b
Anton Rufino <anton.vongola@gmail.com>
2016-12-08 18:43:46 +0800

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+let app = express();
+
+app.set('port', (process.env.PORT || 8000))
+
+app.use('/', express.static(path.join(__dirname, '../frontend')));
+
+app.listen(app.get('port'), () => {
+    console.log('App listening on port ' + app.get(port));
+});
