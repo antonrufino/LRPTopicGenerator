9ba346c5d3e9d6f3cff0174fe8235d903824cd90
Anton Rufino <anton.vongola@gmail.com>
2016-12-08 19:20:43 +0800

    Add router to server.

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ let app = express();
 
 app.set('port', (process.env.PORT || 8000))
 
-app.use('/', express.static(path.join(__dirname, '../frontend')));
+app.use('/', express.static(path.join(__dirname, '/../frontend')));
+app.use(require(__dirname + '/config/router')(express.Router()));
 
 app.listen(app.get('port'), () => {
     console.log('App listening on port ' + app.get('port'));
