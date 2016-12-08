e864b1498b2fb8f5969fd309edde215572bcf012
Anton Rufino <anton.vongola@gmail.com>
2016-12-08 19:21:15 +0800

    Add twitter data.

---
+++ backend/config/twitter.js
@@ -0,0 +1,11 @@
+const Twitter = require('twitter');
+require('dotenv').config({path: __dirname + '/../../.env'});
+
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+module.exports = client;
