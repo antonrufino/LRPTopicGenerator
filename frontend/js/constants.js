5b0c1850a3350f7c16760cca5c3da99cce5ee337
Anton Rufino <anton.vongola@gmail.com>
2016-12-08 21:24:33 +0800

    Move js files to js folder.

---
+++ frontend/js/constants.js
@@ -0,0 +1,25 @@
+var templates = [
+    ["Analysis of the effects of ", " on ", ""],
+    ["The Pros and Cons of replacing ", " with ", ""],
+    ["Why the Philippines should ban ", " and ", ""],
+    ["Why the Philippines should legalize ", " and ", ""],
+    ["Should the government pay attention to ", " instead of ", ""],
+    ["Solving the ", " problem using ", ", ", " and deterministic numerical methods"],
+    ["Edi Wow: An analysis of elitism in the academic fields of ", " and ", ""],
+    ["A mathematical analysis of the effects of ", " on ", ""],
+    ["Constructing computational models of ", " using ", ""],
+    ["An analysis of ", ""],
+    ["On the benefits of ", ""],
+    ["Using ", " as a means of eliminating poverty."],
+    ["Eliminating corruption using ", " and ", ""],
+    ["Feasibility analysis of using ", " as a substitute for ", ": the Philippine context."],
+    ["On the effects of ", " on voter preferences."]
+]
+
+var topics = ["PNoy", "Vice Ganda", "That Thing Called Tadhana", "Fifty Shades of Gray", 
+    "piracy", "hugot", "LRP", "CMSC 56", "CMSC 11", "MATH 17", "my hopes and dreams", 
+    "communism", "existence of forever", "PewDiePie", "anime", "the GTA", "Grande", "Tablazon", "Abello",
+    "Chancy", "finals", "LEs", "papers", "more papers", "UPLB", "DOTA 2", "LOL", "shabu", "n00bs", "Oble", "Mariang Bangag",
+    "Arroyo", "Binay", "Wattpad", "UPCAT", "hugot", "pickup lines", "SU", "Grace Poe", "Binay", "Duterte", "Mar Roxas", 
+    "nuisance candidates", "Fifty Shades of Black", "Deadpool", "Hunger Games", "zero units", 
+    "the fundamental question of \"Reg ka na ba?\""]
